829b00dc6cf395b0ed3ca188a25051aab3ca7b80
Andres Bonett <andresbonettm@gmail.com>
2020-09-11 01:07:54 -0500

    add keyframes animations

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Logo } from './components/Logo'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
-import { GlobalStyle } from './GlobalStyles'
+import { GlobalStyle } from './styles/GlobalStyles'
 
 const App = () => {
   return (
